85ec67594fc400d8fae04b2b96e396cd2dff724a
Roger Tuan <rtuan@fieldmuseum.org>
2020-11-05 13:34:57 -0600

    Hardcode colors to bypass length limit

--- src/index.js
+++ src/index.js
@@ -4,99 +4,108 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
 // Contentful deps
-import { init } from 'contentful-ui-extensions-sdk';
+import {init} from 'contentful-ui-extensions-sdk';
 
 // Forma 36 components
 import '@contentful/forma-36-react-components/dist/styles.css'
 import '@contentful/forma-36-fcss/dist/styles.css'
 import './index.css';
-import { FieldGroup } from '@contentful/forma-36-react-components'
+import {FieldGroup} from '@contentful/forma-36-react-components'
 
 // Custom components
 import Radio from './Radio'
 
 // Create UI Extension component
 class ColorSelector extends React.Component {
-	static propTypes = {
-		sdk: PropTypes.object.isRequired,
-	};
+    static propTypes = {
+        sdk: PropTypes.object.isRequired,
+    };
 
-	detachExternalChangeHandler = null;
+    detachExternalChangeHandler = null;
 
-	constructor(props) {
-		super(props);
-		
-		// Check for colors to be defined
-		let colors = props.sdk.parameters.installation.colors
-		if (!colors) {
-			return props.sdk.notifier.error(`Colors parameter is empty.  This needs to be set 
-				in the UI Extension Settings for the Color Selector.`)
-		}
-		
-		// Check if colors is valid JSON
-		try {
-			colors = JSON.parse(colors)
-		} catch (e) {
-			return props.sdk.notifier.error('The Color Selector "Colors" setting does not contain valid JSON.')
-		}
-		
-		// Set initial state
-		this.state = {
-			colors: colors,
-			value: props.sdk.field.getValue(),
-		};
-	}
+    constructor(props) {
+        super(props);
 
-	// On mount
-	componentDidMount() {
-		this.props.sdk.window.startAutoResizer();
+        // Check for colors to be defined
+        const colors =
+            {
+                "Field Blue": "#0a46e6",
+                "Field Green": "#37816e",
+                "Field Orange": "#F29F77",
+                "Field Purple": "#B274A7",
+                "Field Gray Lighter": "#F0F3F3",
+                "Field Gray Light": "#C9CACC",
+                "Field Gray": "#6a6a71",
+                "Field Gray Darker": "#333336",
+                "Field Black": "#0F0F14",
+                "Success Green": "#53B59E",
+                "Warning Red": "#D44235",
+                "Map Brown": "#663300",
+                "Map Dark Yellow": "#9a7e0b",
+                "Map Light Blue": "#6FB4D6",
+                "Map Light Green": "#AAC38A",
+                "Map Magenta": "#A7197C",
+                "Map Yellow": "#C6AD59",
+            };
 
-		// Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-		this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
-			this.onExternalChange
-		);
-	}
 
-	// On onmount
-	componentWillUnmount() {
-		if (this.detachExternalChangeHandler) {
-			this.detachExternalChangeHandler();
-		}
-	}
+        // Set initial state
+        this.state = {
+            colors: colors,
+            value: props.sdk.field.getValue(),
+        };
+    }
 
-	// Hadle external chages, like from other editors
-	onExternalChange = value => {
-		this.setState({ value });
-	};
+    // On mount
+    componentDidMount() {
+        this.props.sdk.window.startAutoResizer();
 
-	// Handle choices in the radio fields
-	onChange = (value) => {
-		this.setState({ value });
-		if (value) {
-			this.props.sdk.field.setValue(value);
-		} else {
-			this.props.sdk.field.removeValue();
-		}
-	};
-			
-	// Generate the list of radio fields
-	render() {
-		return (
-			<FieldGroup>
-				{ Object.entries(this.state.colors).map(([name, color]) => (
-					<Radio 
-						key={name}
-						name={name}
-						color={color}
-						checked={ this.state.value == name }
-						onClick={ this.onChange }
-						/>
-				))}
-	    </FieldGroup>
-		)
-	}
+        // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+        this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
+            this.onExternalChange
+        );
+    }
+
+    // On onmount
+    componentWillUnmount() {
+        if (this.detachExternalChangeHandler) {
+            this.detachExternalChangeHandler();
+        }
+    }
+
+    // Hadle external chages, like from other editors
+    onExternalChange = value => {
+        this.setState({value});
+    };
+
+    // Handle choices in the radio fields
+    onChange = (value) => {
+        this.setState({value});
+        if (value) {
+            this.props.sdk.field.setValue(value);
+        } else {
+            this.props.sdk.field.removeValue();
+        }
+    };
+
+    // Generate the list of radio fields
+    render() {
+        return (
+            <FieldGroup>
+                {Object.entries(this.state.colors).map(([name, color]) => (
+                    <Radio
+                        key={name}
+                        name={name}
+                        color={color}
+                        checked={this.state.value == name}
+                        onClick={this.onChange}
+                    />
+                ))}
+            </FieldGroup>
+        )
+    }
 }
 
 init(sdk => {
-	ReactDOM.render(<ColorSelector sdk={sdk} />, document.getElementById('root'));
+    ReactDOM.render(<ColorSelector sdk={sdk}/>, document.getElementById('root'));
 });
